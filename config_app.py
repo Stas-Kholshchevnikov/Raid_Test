b2e8f8ae1d0212592555cbfff2298f3f1ba61cb2
Stas Kholshchevnikov <xcd85@mail.ru>
2023-03-07 11:47:35 +0300

    refactor with environment

--- config_app.py
+++ config_app.py
@@ -1,5 +1,11 @@
+import os
+
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:password_26@localhost/raid_test'
+    DB_USER: str = os.getenv('DB_USER')
+    DB_PASSWORD: str = os.getenv('DB_PASSWORD')
+    DB_HOST: str = os.getenv('DB_HOST')
+    DB_NAME: str = os.getenv('DB_NAME')
+    SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
     SQLALCHEMY_TRACK_MODIFICATIONs = False
     RESTX_JSON = {"ensure_ascii": False}
